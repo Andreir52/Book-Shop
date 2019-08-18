897b3eab25aa2f72607b55efa1b3b36133ee3a7b
Andreir52 <andreirizescu97@yahoo.com>
2019-08-18 14:47:35 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Proiect_DAW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proiect_DAW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            UnitOfWork u = new UnitOfWork();
+            //var carte = new Table();
+            //carte.nume = "da";
+            //u.entities.Table.Add(carte);
+            //u.SaveChanges();
+            
+            return View(u);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using Proiect_DAW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class UnitOfWork : IDisposable
+{
+    public DatabaseEntities entities = null;
+    public UnitOfWork()
+    {
+        entities = new DatabaseEntities();
+    }
+
+    // add all the repository handles here
+    List<Books> mbooks = null;
+
+    // add all the repository getters here
+    public List<Books> booklist
+    {
+        get
+        {
+            if (mbooks == null)
+            {
+                mbooks = entities.Books.ToList();
+            }
+            return mbooks;
+        }
+    }
+
+
+
+    public void SaveChanges()
+    {
+        entities.SaveChanges();
+    }
+
+
+    private bool disposed = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this.disposed)
+        {
+            if (disposing)
+            {
+                entities.Dispose();
+            }
+        }
+        this.disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+}
